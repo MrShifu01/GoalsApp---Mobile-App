1bcd686e735cdf9fc607d999a80346ebdd87a2e6
MrShifu01 <stander.christian.com>
2023-07-18 13:13:00 +0200

    basic layout done

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      
+      {/* Text Input and Button */}
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Enter a Goal" />
+        <Button title="Add Goal" />
+      </View>
+
+      {/* List of inputted goals */}
+      <View style={styles.goalsContainer}>
+        <Text>Goal List</Text>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    appContainer: {
+      flex: 1,
+      paddingTop: 50,
+      paddingHorizontal: 16,
+    },
+
+    inputContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      marginBottom: 24,
+      borderBottomWidth: 1,
+      borderBottomColor: '#cccccc',
+    },
+
+    textInput: {
+      width: '70%',
+      borderColor: '#cccccc',
+      borderWidth: 1,
+      padding: 8,
+      marginBottom: 8, 
+      marginRight: 8, 
+    },
+
+    goalsContainer: {
+      flex: 4,
+    }
+
 });
